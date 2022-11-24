3c25690fd25868786b0e03b1bae01a144d723b55
Mauricio-Natacci <85077684+Mauricio-Natacci@users.noreply.github.com>
2022-11-23 23:39:22 -0300

    test

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(errorHandlerMiddleware)
 
 db.connectToDatabase()
   .then(function () {
-    app.listen(port)
+    app.listen(process.env.PORT || 3000)
   })
   .catch(function (error) {
     console.log('Failed to connect to the database!')
