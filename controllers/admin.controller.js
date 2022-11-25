af0b3f441e8a5206af7a04e4c0443b9f8bc0638f
Mauricio-Natacci <85077684+Mauricio-Natacci@users.noreply.github.com>
2022-11-25 13:50:15 -0300

    added s3

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,12 +1,11 @@
 const Product = require('../models/product.model')
 const Order = require('../models/order.model')
 
-
 async function getProducts(req, res, next) {
   try {
     const products = await Product.showProducts()
-    res.render('admin/products/all-products', {products})
-  } catch(error) {
+    res.render('admin/products/all-products', { products })
+  } catch (error) {
     next(error)
     return
   }
@@ -19,12 +18,12 @@ function getNewProduct(req, res) {
 async function createNewProduct(req, res, next) {
   const product = new Product({
     ...req.body,
-    image: req.file.filename
+    image: req.file.key
   })
 
   try {
     await product.save()
-  } catch(error) {
+  } catch (error) {
     next(error)
     return
   }
@@ -34,8 +33,8 @@ async function createNewProduct(req, res, next) {
 async function getUpdateProduct(req, res, next) {
   try {
     const product = await Product.findById(req.params.id)
-    res.render('admin/products/update-product', {product})
-  } catch(error) {
+    res.render('admin/products/update-product', { product })
+  } catch (error) {
     next(error)
   }
 }
@@ -52,54 +51,50 @@ async function updateProduct(req, res, next) {
 
   try {
     await product.save()
-  } catch(error){
+  } catch (error) {
     next(error)
     return
   }
   res.redirect('/admin/products')
 }
 
-  async function deleteItem(req, res, next) {
-    let product
-    try {
-      product = await Product.findById(req.params.id)
-      await product.remove()
-    } catch(error){
-      next(error)
-      return
-    }
-
-    res.json({message: 'item deleted!'})
-
+async function deleteItem(req, res, next) {
+  let product
+  try {
+    product = await Product.findById(req.params.id)
+    await product.remove()
+  } catch (error) {
+    next(error)
+    return
   }
 
+  res.json({ message: 'item deleted!' })
+}
 
-  async function getOrders(req, res, next) {
-    try {
-      const orders = await Order.findAll();
-      res.render('admin/orders/admin-orders', {
-        orders: orders
-      });
-    } catch (error) {
-      next(error);
-    }
+async function getOrders(req, res, next) {
+  try {
+    const orders = await Order.findAll()
+    res.render('admin/orders/admin-orders', {
+      orders: orders
+    })
+  } catch (error) {
+    next(error)
   }
+}
 
-  async function updateStatus(req, res, next) {
-    const orderId = req.params.id;
-    const newStatus = req.body.newStatus;
-    try {
+async function updateStatus(req, res, next) {
+  const orderId = req.params.id
+  const newStatus = req.body.newStatus
+  try {
+    const order = await Order.findById(orderId)
 
-      const order = await Order.findById(orderId);
-
-      order.status = newStatus;
-      await order.save()
-      res.json({ message: 'Order updated', newStatus: newStatus });
-    } catch (error) {
-      next(error);
-    }
+    order.status = newStatus
+    await order.save()
+    res.json({ message: 'Order updated', newStatus: newStatus })
+  } catch (error) {
+    next(error)
   }
-
+}
 
 module.exports = {
   getProducts: getProducts,
--- middlewares/image-upload.js
+++ middlewares/image-upload.js
@@ -1,15 +1,20 @@
-const multer = require('multer');
-const uuid = require('uuid').v4;
+const multer = require('multer')
+const uuid = require('uuid').v4
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3')
 
 const upload = multer({
-  storage: multer.diskStorage({
-    destination: 'product-data/images',
-    filename: function(req, file, cb) {
-      cb(null, uuid() + '-' + file.originalname);
+  storage: multerS3({
+    s3: new aws.S3(),
+    bucket: 'myonlineshop0',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: function (req, file, cb) {
+      cb(null, uuid() + '-' + file.originalname)
     }
   })
-});
+})
 
-const configuredMulterMiddleware = upload.single('image');
+const configuredMulterMiddleware = upload.single('image')
 
-module.exports = configuredMulterMiddleware;
+module.exports = configuredMulterMiddleware
